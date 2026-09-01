4a1a98ad040ce95492961f3695c9308057749fe0
Cesahz <espinolac999@gmail.com>
2026-09-01 12:27:38 -0300

    feat: implementar factory pattern con create_app

---
+++ app/__init__.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from .config import Config
+from .extensions import db, bcrypt, limiter, csrf, server_session
+from .models.models import User, Role #importar modelo para que sqlalchemy reconozca al crear tabla
+import os
+
+def create_app(config_class=Config):
+    #inicializa la aplicacion flask
+    app = Flask(__name__)
+    app.config.from_object(config_class) #lee todos los atributos de la clase
+
+    #crear el directorio de sesiones si no existe
+    os.makedirs(app.config['SESSION_FILE_DIR'], exist_ok=True)
+
+    #vincula las extensiones
+    db.init_app(app)
+    bcrypt.init_app(app)
+    limiter.init_app(app)
+    if csrf:
+        csrf.init_app(app)
+    server_session.init_app(app)
+
+    #contexto de la app para operaciones de base de datos iniciales
+    with app.app_context():
+        #crea el archivo sqlite y las tablas si no existen
+        db.create_all()
+        
+        #creacion automatica de roles base si la tabla esta vacia
+        if not Role.query.first():
+            rol_usuario = Role(name='usuario')
+            rol_admin = Role(name='administrador')
+            db.session.add_all([rol_usuario, rol_admin]) #session es como el cursor
+            db.session.commit()
+
+    return app
